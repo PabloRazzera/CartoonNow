fd82fb9f92a8b8cc6026cc94c7c921dddbe77a90
PabloRazzera <pablohenriquerazzera123@gmail.com>
2026-06-18 20:43:27 -0300

    Implement user profile display and logout functionality

--- js/perfil.js
+++ js/perfil.js
@@ -1,1 +1,75 @@
+const usuario =
+JSON.parse(
+localStorage.getItem("usuarioLogado")
+);
 
+if(usuario){
+
+```
+document.getElementById(
+    "nomeUsuario"
+).innerText = usuario.nome;
+```
+
+}
+
+/* MINHA LISTA */
+
+const lista =
+JSON.parse(
+localStorage.getItem("minhaLista")
+) || [];
+
+const listaDiv =
+document.getElementById("minhaLista");
+
+lista.forEach(item => {
+
+```
+listaDiv.innerHTML += `
+    <div class="item-perfil">
+        ${item.nome}
+    </div>
+`;
+```
+
+});
+
+/* CONTINUAR ASSISTINDO */
+
+const nome =
+localStorage.getItem("continuarNome");
+
+const tempo =
+localStorage.getItem("continuarTempo");
+
+if(nome){
+
+```
+document.getElementById(
+    "continuarAssistindo"
+).innerHTML = `
+    <div class="item-perfil">
+        ${nome}
+        <br>
+        Tempo salvo: ${tempo}s
+    </div>
+`;
+```
+
+}
+
+/* LOGOUT */
+
+function logout(){
+
+```
+localStorage.removeItem(
+    "usuarioLogado"
+);
+
+window.location.href =
+    "login.html";
+```
+
+}
