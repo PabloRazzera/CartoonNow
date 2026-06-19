938a64263165c2b7aa9de37ca3dc79b0c8dfcc7b
PabloRazzera <pablohenriquerazzera123@gmail.com>
2026-06-18 21:03:52 -0300

    Add serie.js to load series and episodes

---
+++ js/serie.js
@@ -0,0 +1,42 @@
+const params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+function carregarSerie(){
+
+    const db = getDB();
+
+    const serie = db.series.find(s => s.id === id);
+
+    if(!serie) return;
+
+    document.querySelector(".serie-info h1").innerText = serie.nome;
+    document.querySelector(".serie-capa").src = serie.capa;
+
+    const eps = db.episodios.filter(e => e.serieId === id);
+
+    const container = document.querySelector(".temporadas");
+
+    container.innerHTML = "<h2>Episódios</h2>";
+
+    eps.forEach(e => {
+
+        container.innerHTML += `
+            <div class="episodio">
+                <div>
+                    <h3>${e.titulo}</h3>
+                    <p>${e.duracao}</p>
+                </div>
+
+                <button onclick="assistir('${e.id}')">
+                    Assistir
+                </button>
+            </div>
+        `;
+    });
+}
+
+function assistir(id){
+    window.location.href = "episodio.html?id=" + id;
+}
+
+carregarSerie();
