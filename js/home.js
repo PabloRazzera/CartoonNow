9730dd6f6855f120fe31181540eece68c0eec806
PabloRazzera <pablohenriquerazzera123@gmail.com>
2026-06-18 21:01:50 -0300

    Add home page loading and series display
    Implement home page loading functionality with series display.

---
+++ js/home.js
@@ -0,0 +1,26 @@
+function carregarHome(){
+
+    const db = getDB();
+
+    const container = document.querySelector(".row");
+
+    if(!container) return;
+
+    container.innerHTML = "";
+
+    db.series.forEach(s => {
+
+        container.innerHTML += `
+            <div class="card" onclick="abrirSerie('${s.id}')">
+                <img src="${s.capa}">
+                <span>${s.nome}</span>
+            </div>
+        `;
+    });
+}
+
+function abrirSerie(id){
+    window.location.href = "serie.html?id=" + id;
+}
+
+carregarHome();
