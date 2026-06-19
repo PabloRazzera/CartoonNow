b61b0059c1cccb865f24434b55d607657bb49c99
PabloRazzera <pablohenriquerazzera123@gmail.com>
2026-06-18 21:50:47 -0300

    Refactor carregarContinuar and irContinuar functions

--- js/continuar.js
+++ js/continuar.js
@@ -6,21 +6,19 @@ function carregarContinuar(){
     const data = JSON.parse(localStorage.getItem("continuar"));
 
     if(!data){
-        container.innerHTML = "<p>Nada assistido ainda</p>";
+        container.innerHTML = "<p>Nada assistido</p>";
         return;
     }
 
     container.innerHTML = `
-        <div class="card" onclick="irContinuar('${data.id}')">
-            <div style="padding:10px">
-                <h3>${data.titulo}</h3>
-                <p>Retomar em ${Math.floor(data.tempo)}s</p>
-            </div>
+        <div class="card" onclick="ir('${data.id}')">
+            <h3>${data.titulo}</h3>
+            <p>Retomar em ${Math.floor(data.tempo)}s</p>
         </div>
     `;
 }
 
-function irContinuar(id){
+function ir(id){
     window.location.href = "episodio.html?id=" + id;
 }
 
