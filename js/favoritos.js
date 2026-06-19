f6d6119a5c4122d83d4a2bb3abc92cfef3dbf019
PabloRazzera <pablohenriquerazzera123@gmail.com>
2026-06-18 21:59:45 -0300

    Update favoritos.js

--- js/favoritos.js
+++ js/favoritos.js
@@ -2,20 +2,9 @@ function adicionarMinhaLista(serie){
 
     let lista = JSON.parse(localStorage.getItem("minhaLista")) || [];
 
-    const existe = lista.find(s => s.id === serie.id);
-
-    if(!existe){
+    if(!lista.find(s => s.id === serie.id)){
         lista.push(serie);
     }
 
     localStorage.setItem("minhaLista", JSON.stringify(lista));
-
-    alert("Adicionado à Minha Lista!");
-}
-
-function assistir(id){
-
-    localStorage.setItem("episodioAtual", id);
-
-    window.location.href = "episodio.html?id=" + id;
 }
